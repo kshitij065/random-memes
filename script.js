52f3dca0ca3e8a41cd305489497869aee014c12d
kshitij065 <129906291+kshitij065@users.noreply.github.com>
2023-04-05 06:10:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// Array of meme image URLs
-var memeUrls = [
-  "https://example.com/meme1.jpg",
-  "https://example.com/meme2.jpg",
-  "https://example.com/meme3.jpg",
-  // Add more meme image URLs here
-];
-
-// Function to generate a random meme
-function generateMeme() {
-  var memeIndex = Math.floor(Math.random() * memeUrls.length);
-  var memeImage = document.getElementById("meme-image");
-  memeImage.src = memeUrls[memeIndex];
+function getRandomMeme() {
+    var memeImage = document.getElementById('memeImage');
+    memeImage.src = 'https://meme-api.herokuapp.com/gimme';
 }
-
-// Add event listener to the "Generate" button
-var generateButton = document.getElementById("generate-button");
-generateButton.addEventListener("click", generateMeme);
